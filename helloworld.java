aca78e02611830fef198afbc52efa4e12bc63cb2
root <root@ip-172-31-30-132.us-east-2.compute.internal>
2019-02-14 05:03:00 +0000

    Added print statement

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 	// print "hello world"
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+	//print "welcome to git" 
     }
 }
